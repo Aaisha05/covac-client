050714b94a2ca7d2bdbf55a7d3cee2210b94ef9d
Aaisha Nabeela <aaishanabeelas@gmail.com>
2024-02-10 13:23:37 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AdminDashboard from './pages/Admin/dashboard'; 
 import UsersDashboard from './pages/Users/dashboard'; 
+import Home1 from './pages/Main/home';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/admin/dashboard" element={<AdminDashboard />} />      
         <Route path="/users/dashboard" element={<UsersDashboard />} />      
+        <Route path="/" element={<Home1 />} />      
       </Routes>
     </Router>
   );
