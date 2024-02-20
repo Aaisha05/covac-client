183ba7750e08e278a4d0fa526420ca363971eb50
Aaisha Nabeela <aaishanabeelas@gmail.com>
2024-02-20 23:43:04 +0530

    newww

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql2 = require('mysql2');
-
+require('dotenv').config(); //spelling crrct dana
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -31,16 +31,12 @@ app.post('/signup', (req, res) => {
 app.post('/login', (req, res) => {
   const sql = "SELECT * FROM users WHERE email = ? AND password = ?";
   const values = [req.body.email, req.body.password];
-
   db.query(sql, values, (err, results) => {
     if (err) {
       return res.json(err);
     }
-
     if (results.length > 0) {
-      const user = results[0];
-
-      
+      const user = results[0];     
       if (user.email === 'root@gmail.com' && user.password === 'root') {
         return res.json({ message: 'Login successful', redirect: '/admin/dashboard' });
       } else {
@@ -197,7 +193,7 @@ app.route('/user/slots')
     });
   });
 
-
-app.listen(5000, () => {
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
   console.log("Listening");
 });
